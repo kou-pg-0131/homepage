c911274b604bcf5ae8462f84b284f27213209a05
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-17 08:08:37 +0900

    CssBaseline を適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import { CssBaseline } from '@material-ui/core';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <Component {...pageProps}/>
+    <React.Fragment>
+      <CssBaseline/>
+      <Component {...pageProps}/>
+    </React.Fragment>
   );
 };
 
