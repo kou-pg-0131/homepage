c7bb7f164e4a66b4a19de85632f3d8b38b7e1018
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-17 11:40:52 +0900

    eslintrc を更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  parser: '@typescript-eslint/parser',
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'quotes': ['error', 'single'],
+  },
 };
