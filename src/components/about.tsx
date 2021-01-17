f3fd88532667223ad3ed29a07034c7740d825e21
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-17 15:44:40 +0900

    profile.png を public/images 配下に移動

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -5,7 +5,7 @@ export const About: React.FC = () => {
   return (
     <div className={styles.about}>
       <div className={styles.about__user}>
-        <img src='/profile.png' alt='Koki Sato' width={150}/>
+        <img src='/images/profile.png' alt='Koki Sato' width={150}/>
         <p>Koki Sato</p>
       </div>
       <p className={styles.about__tagline}>Developer</p>
