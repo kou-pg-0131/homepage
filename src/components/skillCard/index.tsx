98697bbac4b00584f736b6d405bd4088d86e3d71
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-18 11:47:49 +0900

    ActionArea を追加

--- src/components/skillCard/index.tsx
+++ src/components/skillCard/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardActionArea, CardContent } from '@material-ui/core';
 import styles from './index.module.scss';
 import { ExternalLink } from '..';
 
@@ -12,13 +12,15 @@ type Props = {
 export const SkillCard: React.FC<Props> = (props: Props) => {
   return (
     <ExternalLink className={styles.link} href={props.href}>
-      <Card className={styles.card}>
-        <CardContent>
-          <img className={styles.img} src={props.imgSrc} alt={props.name}/>
-          <div className={styles.name}>
-            {props.name}
-          </div>
-        </CardContent>
+      <Card>
+        <CardActionArea>
+          <CardContent className={styles.content}>
+            <img className={styles.img} src={props.imgSrc} alt={props.name}/>
+            <div className={styles.name}>
+              {props.name}
+            </div>
+          </CardContent>
+        </CardActionArea>
       </Card>
     </ExternalLink>
   );
