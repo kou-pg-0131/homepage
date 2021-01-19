ec64038162a4943309d8b69ee4abe73e924f8f2b
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-19 09:13:31 +0900

    スクロールで表示するようにした

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styles from './index.module.scss';
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, useScrollTrigger, Slide } from '@material-ui/core';
 import { Link } from '../../components';
 
 export const Header: React.FC = () => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 250,
+  });
+
   return (
-    <AppBar className={styles.root}>
-      <Toolbar className={styles.toolbar}>
-        <ul className={styles.menu}>
-          <li className={styles.menuItem}><Link to='skills'>Skills</Link></li>
-          <li className={styles.menuItem}><Link to='portfolios'>Portfolios</Link></li>
-          <li className={styles.menuItem}><Link to='history'>History</Link></li>
-        </ul>
-      </Toolbar>
-    </AppBar>
+    <Slide in={trigger} direction='down'>
+      <AppBar className={styles.root}>
+        <Toolbar className={styles.toolbar}>
+          <ul className={styles.menu}>
+            <li className={styles.menuItem}><Link to='skills'>Skills</Link></li>
+            <li className={styles.menuItem}><Link to='portfolios'>Portfolios</Link></li>
+            <li className={styles.menuItem}><Link to='history'>History</Link></li>
+          </ul>
+        </Toolbar>
+      </AppBar>
+    </Slide>
   );
 };
