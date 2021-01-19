25b526c9c906b3f5c707f26e6030651ad244a8b2
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-01-19 21:39:43 +0900

    footer をリファクタ

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './index.module.scss';
+import { ExternalLink } from '../../components';
 
 export const Footer: React.FC = () => {
   return (
@@ -7,6 +9,16 @@ export const Footer: React.FC = () => {
       <small>
         &copy;2021
       </small>
+      <ul>
+        <li className={styles.listItem}>
+          <ExternalLink className={styles.link} href='https://github.com/kou-pg-0131/homepage'>View on GitHub</ExternalLink>
+        </li>
+        <li className={styles.listItem}>
+          <Link href='/privacyPolicy'>
+            <a className={styles.link} href='https://github.com/kou-pg-0131/homepage'>プライバシーポリシー</a>
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 };
