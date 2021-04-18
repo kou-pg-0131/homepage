142220e246a8a50ae7f4d1bcd21958b0489b4cda
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:27:04 +0900

    socialListItem/index.module.scss を削除

--- src/components/socialListItem/index.tsx
+++ src/components/socialListItem/index.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import styles from './index.module.scss';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Social } from '../../domain';
 import { ExternalLink } from '..';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    listItem: {
+      margin: '0 12px',
+    },
+    link: {
+      transition: '0.2s',
+      '&:hover': {
+        opacity: 0.5,
+      },
+    },
+    img: {
+      height: 40,
+      width: 40,
+    },
+  }),
+);
 
 type Props = {
   social: Social;
 };
 
 export const SocialListItem: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
-    <li className={styles.listItem}>
-      <ExternalLink className={styles.link} href={props.social.href}>
-        <img className={styles.img} src={props.social.imgSrc} alt={props.social.name}/>
+    <li className={classes.listItem}>
+      <ExternalLink className={classes.link} href={props.social.href}>
+        <img className={classes.img} src={props.social.imgSrc} alt={props.social.name}/>
       </ExternalLink>
     </li>
   );
