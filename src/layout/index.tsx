de1af9758da9d4750ecd8999f7fa2525893127d2
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:37:51 +0900

    layout/index.module.scss を削除

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import styles from './index.module.scss';
 import Head from 'next/head';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import { Header } from './header';
 import { Footer } from './footer';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    main: {
+      paddingTop: 80,
+    },
+  }),
+);
 
 type Props = {
   children: React.ReactNode;
@@ -11,13 +19,15 @@ type Props = {
 };
 
 export const Layout: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <Head>
         <title>{props.title && `${props.title} | `}Koki Sato</title>
       </Head>
       <Header hideMenu={props.hideMenu}/>
-      <main className={styles.main}>
+      <main className={classes.main}>
         {props.children}
       </main>
       <Footer/>
