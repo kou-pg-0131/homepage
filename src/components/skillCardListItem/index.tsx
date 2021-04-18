e1aed9a26642b541ff1750e309bf835ba2ca375b
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:23:45 +0900

    skillCardListItem/index.module.scss を削除

--- src/components/skillCardListItem/index.tsx
+++ src/components/skillCardListItem/index.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import { Card, CardActionArea, CardContent, Grid } from '@material-ui/core';
-import styles from './index.module.scss';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { ExternalLink } from '..';
 import { Skill } from '../../domain';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    link: {
+      display: 'block',
+      width: '100%',
+    },
+    content: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    img: {
+      marginBottom: theme.spacing(2),
+      width: 60,
+      height: 60,
+    },
+    name: {
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  }),
+);
 
 type Props = {
   skill: Skill;
 };
 
 export const SkillCardListItem: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
     <Grid item xs={6} sm={3}>
-      <ExternalLink className={styles.link} href={props.skill.href}>
+      <ExternalLink className={classes.link} href={props.skill.href}>
         <Card>
           <CardActionArea>
-            <CardContent className={styles.content}>
-              <img className={styles.img} src={props.skill.imgSrc} alt={props.skill.name}/>
-              <div className={styles.name}>
+            <CardContent className={classes.content}>
+              <img className={classes.img} src={props.skill.imgSrc} alt={props.skill.name}/>
+              <div className={classes.name}>
                 {props.skill.name}
               </div>
             </CardContent>
