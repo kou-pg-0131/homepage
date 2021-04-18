db7602c660ae0dc5972ee6607be398e4949efab5
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:32:52 +0900

    footer/index.module.scss を削除

--- src/layout/footer/index.tsx
+++ src/layout/footer/index.tsx
@@ -1,21 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from './index.module.scss';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { ExternalLink } from '../../components';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    footer: {
+      alignItems: 'center',
+      display: 'flex',
+      justifyContent: 'center',
+      padding: theme.spacing(2),
+      flexDirection: 'column',
+    },
+    listItem: {
+      fontSize: 12,
+      marginBottom: 4,
+      textAlign: 'center',
+    },
+    link: {
+      transition: '0.2s',
+      '&:hover': {
+        opacity: 0.5,
+      },
+    },
+  }),
+);
+
 export const Footer: React.VFC = () => {
+  const classes = useStyles();
+
   return (
-    <footer className={styles.footer}>
+    <footer className={classes.footer}>
       <small>
         &copy;2021
       </small>
       <ul>
-        <li className={styles.listItem}>
-          <ExternalLink className={styles.link} href='https://github.com/kou-pg-0131/homepage'>View on GitHub</ExternalLink>
+        <li className={classes.listItem}>
+          <ExternalLink className={classes.link} href='https://github.com/kou-pg-0131/homepage'>View on GitHub</ExternalLink>
         </li>
-        <li className={styles.listItem}>
+        <li className={classes.listItem}>
           <Link href='/privacyPolicy'>
-            <a className={styles.link} href='https://github.com/kou-pg-0131/homepage'>プライバシーポリシー</a>
+            <a className={classes.link} href='https://github.com/kou-pg-0131/homepage'>プライバシーポリシー</a>
           </Link>
         </li>
       </ul>
