d26c91f24a8f9cc4e006beeb948d14b5d2fb7180
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:20:41 +0900

    section/index.module.scss を削除

--- src/components/section/index.tsx
+++ src/components/section/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import styles from './index.module.scss';
 import { Container } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    section: {
+      margin: '25px 0',
+    },
+    title: {
+      fontSize: 28,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  }),
+);
 
 type Props = {
   title?: string;
@@ -8,11 +21,13 @@ type Props = {
 };
 
 export const Section: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
-    <section className={styles.root}>
+    <section className={classes.section}>
       <Container maxWidth='md'>
         {props.title && (
-          <h2 className={styles.title}>{props.title}</h2>
+          <h2 className={classes.title}>{props.title}</h2>
         )}
         {props.children}
       </Container>
