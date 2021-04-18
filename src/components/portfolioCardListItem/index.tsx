ca7e43acf000b1976f42ac799a4ae6f73e991e0f
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:18:42 +0900

    portfolioCardListItem/index.module.scss を削除

--- src/components/portfolioCardListItem/index.tsx
+++ src/components/portfolioCardListItem/index.tsx
@@ -1,15 +1,62 @@
 import React from 'react';
-import styles from './index.module.scss';
 import { ExternalLink } from '..';
 import { Avatar, Card, CardContent, CardActionArea, CardMedia, Grid } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { AvatarGroup } from '@material-ui/lab';
 import { Portfolio, Skill } from '../../domain';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    img: {
+      height: 300,
+      [theme.breakpoints.down('lg')]: {
+        height: 280,
+      },
+      [theme.breakpoints.down('md')]: {
+        height: 200,
+      },
+    },
+    avatarGroup: {
+      marginBottom: theme.spacing(2),
+    },
+    avatarLink: {
+      border: '1px solid #ddd !important',
+      borderRadius: '50%',
+    },
+    avatar: {
+      backgroundColor: '#fff',
+      height: 25,
+      width: 25,
+    },
+    avatarImg: {
+      objectFit: 'fill',
+    },
+    content: {
+      paddingTop: 0,
+    },
+    title: {
+      fontSize: 22,
+      margin: '7px 0',
+    },
+    description: {
+      marginBottom: theme.spacing(2),
+    },
+    githubLink: {
+      transition: '0.2s',
+      '&:hover': {
+        opacity: 0.5,
+      },
+    },
+  }),
+);
 
 type Props = {
   portfolio: Portfolio;
 };
 
 export const PortfolioCardListItem: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
     <Grid item xs={12}>
       <Card raised>
@@ -17,23 +64,23 @@ export const PortfolioCardListItem: React.VFC<Props> = (props: Props) => {
           <CardActionArea>
             <ExternalLink href={props.portfolio.url}>
               <CardMedia
-                className={styles.img}
+                className={classes.img}
                 image={props.portfolio.imgSrc}
               />
             </ExternalLink>
           </CardActionArea>
         )}
-        <CardContent className={styles.content}>
-          <h4 className={styles.title}>{props.portfolio.title}</h4>
-          <AvatarGroup className={styles.avatarGroup} max={100}>
+        <CardContent className={classes.content}>
+          <h4 className={classes.title}>{props.portfolio.title}</h4>
+          <AvatarGroup className={classes.avatarGroup} max={100}>
             {(props.portfolio.skills as unknown as Skill[]).sort((a, b) => a.name < b.name ? -1 : 1).map(skill => (
-              <ExternalLink key={skill.name} className={styles.avatarLink} href={skill.href}>
-                <Avatar className={styles.avatar} imgProps={{ className: styles.avatarImg }} src={skill.imgSrc} alt={skill.name}/>
+              <ExternalLink key={skill.name} className={classes.avatarLink} href={skill.href}>
+                <Avatar className={classes.avatar} imgProps={{ className: classes.avatarImg }} src={skill.imgSrc} alt={skill.name}/>
               </ExternalLink>
             ))}
           </AvatarGroup>
           <p>{props.portfolio.description}</p>
-          <ExternalLink className={styles.githubLink} href={props.portfolio.githubUrl}>
+          <ExternalLink className={classes.githubLink} href={props.portfolio.githubUrl}>
             View on GitHub
           </ExternalLink>
         </CardContent>
