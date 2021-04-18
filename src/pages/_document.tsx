ab2b8a3f45ca78cf5d7cddbdef01b151cfdfc610
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 21:17:16 +0900

    material-ui/core/styles に必要な設定を追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
 
 export default class Document extends NextDocument {
   render(): JSX.Element {
@@ -44,3 +45,20 @@ export default class Document extends NextDocument {
     );
   }
 }
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await NextDocument.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
+};
