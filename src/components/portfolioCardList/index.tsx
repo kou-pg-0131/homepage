92ec444815fdb13fa20ca5e4a0b86f8f1cffc8c7
kou-pg-0131 <kou.pg.0131@gmail.com>
2021-04-18 16:13:02 +0900

    portfolioCardList/index.module.scss を削除

--- src/components/portfolioCardList/index.tsx
+++ src/components/portfolioCardList/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import styles from './index.module.scss';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      marginBottom: theme.spacing(2),
+    },
+  }),
+);
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const PortfolioCardList: React.VFC<Props> = (props: Props) => {
+  const classes = useStyles();
+
   return (
-    <Grid container className={styles.root} spacing={4}>
+    <Grid container className={classes.container} spacing={4}>
       {props.children}
     </Grid>
   );
